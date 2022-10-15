2a000e029da6b72f6723a85212c1a10f7bbe8db0
Yeonsu Park <yspark-dblab@dblab.postech.ac.kr>
2022-10-15 19:51:15 -0400

    Clean scripts

--- scripts/exp-queryset-size.sh
+++ scripts/exp-queryset-size.sh
@@ -9,25 +9,15 @@ for dataset in bra ebay; do
 	if [ ${dataset} = bra ]; then
 		for num_queries in ${num_queries_list1[@]}; do
 			for method in qaad sparks sparku; do
-        for iter in {1..10}; do
-					result_file=${dir}/${method}-${dataset}-r-${num_rows}-p-${num_partitions}-q-${num_queries}.txt
-					check=$(cat ${result_file} | grep "Results" | wc -l)
-					if [ ${check} = 0 ]; then # check if the result file alreay exists
-						./run-${method}-yarn.sh ${dataset} ${num_rows} ${num_partitions} ${num_queries} > ${result_file}
-					fi
-        done
+				result_file=${dir}/${method}-${dataset}-r-${num_rows}-p-${num_partitions}-q-${num_queries}.txt
+				./run-${method}-yarn.sh ${dataset} ${num_rows} ${num_partitions} ${num_queries} > ${result_file}
 			done
 		done
 	else
 	  for num_queries in ${num_queries_list2[@]}; do
 			for method in qaad sparks sparku; do
-        for iter in {1..10}; do
-					result_file=${dir}/${method}-${dataset}-r-${num_rows}-p-${num_partitions}-q-${num_queries}.txt
-					check=$(cat ${result_file} | grep "Results" | wc -l)
-					if [ ${check} = 0 ]; then # check if the result file alreay exists
-						./run-${method}-yarn.sh ${dataset} ${num_rows} ${num_partitions} ${num_queries} > ${result_file}
-					fi
-				done
+				result_file=${dir}/${method}-${dataset}-r-${num_rows}-p-${num_partitions}-q-${num_queries}.txt
+				./run-${method}-yarn.sh ${dataset} ${num_rows} ${num_partitions} ${num_queries} > ${result_file}
 			done
 		done
   fi
